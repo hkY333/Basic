101a6794ed7997f6159e46123ec81426d49b6b23
KimYounghun <186786431+hkY333@users.noreply.github.com>
2025-06-04 21:14:34 +0900

    feat : PopupBank 스크립트 완성

--- ATM/Assets/Scripts/PopupBank.cs
+++ ATM/Assets/Scripts/PopupBank.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class PopupBank : MonoBehaviour
 {
-    [SerializeField] private GameObject depositBut;
-    [SerializeField] private GameObject withdrawalBut;
+    [SerializeField] private GameObject depositBtn;
+    [SerializeField] private GameObject withdrawalBtn;
     [SerializeField] private GameObject depositUI;
     [SerializeField] private GameObject withdrawalUI;
 
     public void OpenDepositUI()
     {
-
+        depositBtn.SetActive(false);
+        withdrawalBtn.SetActive(false);
+        depositUI.SetActive(true);
     }
 
-    public void CloseDepositUI() 
+    public void OpenWithdrawalUI() 
     {
+        depositBtn.SetActive(false);
+        withdrawalBtn.SetActive(false);
+        withdrawalUI.SetActive(true);
+    }
 
+    public void CloseDepositUI()
+    {
+        depositBtn.SetActive(true);
+        withdrawalBtn.SetActive(true);
+        depositUI.SetActive(false);
+    }
+
+    public void CloseWithdrawalUI()
+    {
+        depositBtn.SetActive(true);
+        withdrawalBtn.SetActive(true);
+        withdrawalUI.SetActive(false);
     }
 }
