ce1d8d421929226f659c06391054368ef0542f5f
KimYounghun <186786431+hkY333@users.noreply.github.com>
2025-06-04 15:06:09 +0900

    Feat : UserData 스크립트 추가
    스크립터블 오브젝트로 만듦

---
+++ ATM/Assets/Scripts/UserData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData : ScriptableObject
+{
+    public string name;
+    public int balance;
+    public int cash;
+}
