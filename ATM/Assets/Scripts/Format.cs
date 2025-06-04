d46a491ad22b686ec567385c620a19012b350a8f
KimYounghun <186786431+hkY333@users.noreply.github.com>
2025-06-04 14:50:17 +0900

    Feat : 천단위 콤마 기능인 Format 스크립트 추가
    + NowMoney → NowBalance 이름 수정
    NowBalance, NowCash 오브젝트에 바인딩

---
+++ ATM/Assets/Scripts/Format.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEngine;
+
+public class Format : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private int num;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = num.ToString("#,###");
+        //text.text = string.Format("{0:#,###}", text.text);
+    }
+}
