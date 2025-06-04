2cd8f601980624347bc282084a0d7ce800d2b977
KimYounghun <186786431+hkY333@users.noreply.github.com>
2025-06-04 21:03:19 +0900

    Feat : PopupBank 스크립트 틀 생성

---
+++ ATM/Assets/Scripts/PopupBank.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupBank : MonoBehaviour
+{
+    [SerializeField] private GameObject depositBut;
+    [SerializeField] private GameObject withdrawalBut;
+    [SerializeField] private GameObject depositUI;
+    [SerializeField] private GameObject withdrawalUI;
+
+    public void OpenDepositUI()
+    {
+
+    }
+
+    public void CloseDepositUI() 
+    {
+
+    }
+}
